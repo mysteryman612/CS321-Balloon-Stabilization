7c01fca2b5eb9bb990674be5c9523ced6cbc0afc
Unknown <saintpeterthegreat@gmail.com>
2018-09-20 08:25:14 -0400

    Initial Stabilization code

---
+++ Stabilization Software/src/StabilizationOnly.java
@@ -0,0 +1,11 @@
+
+public class StabilizationOnly {
+/*
+ * this code only stabilizes the craft, without any ability for heading changes
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
