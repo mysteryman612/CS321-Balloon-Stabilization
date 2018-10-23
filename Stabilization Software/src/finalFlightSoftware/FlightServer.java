d9d5b6f47c9a4dba60d7ac579488ac1720f9087d
mysteryman612 <saintpeterthegreat@gmail.com>
2018-10-23 09:32:35 -0400

    Documented the code for the main flight server ¯\_(ツ)_/¯

--- Stabilization Software/src/finalFlightSoftware/FlightServer.java
+++ Stabilization Software/src/finalFlightSoftware/FlightServer.java
@@ -1,3 +1,13 @@
+/*
+ * Main flight software:
+ * Version 0.6.9
+ * 
+ * This code is the main part of the server which listens
+ * to incoming connections and spins off a thread to handle the 
+ * request from the client in order to keep our well-known port
+ * from being tied up with other requests
+ */
+
 package finalFlightSoftware;
 
 import java.io.IOException;
@@ -11,20 +21,32 @@ public class FlightServer {
 
 		while (true) {
 			try {
+
+				// set up the well-known port and listen for new connections
 				welcomeSocket = new ServerSocket(32118);
 				System.out.println("accepting call ...");
+
+				// received a connection request from a client
 				connectionSocket = welcomeSocket.accept();
+
+				// start a new thread to handle the connection and resume listening
 				Thread connection = new Thread(new ConnectionHandler(connectionSocket));
 				connection.start();
-
 				System.out.println("connected ...");
 
 			} catch (IOException e) {
+				//some IO error occured, probably something with the internet.
+				//pretend like nothing happened and move on
 				System.out.println("Something went wrong with the server");
 			} finally {
-				try {
-					welcomeSocket.close();
+				
+				//close the well-known port and free up the network resource
+				try {				
+					if (welcomeSocket != null) {
+						welcomeSocket.close();
+					}
 				} catch (IOException e) {
+					//something went wrong when closing the server so you're SOL
 					System.out.println("Something went wrong when closing server...");
 				}
 			}
